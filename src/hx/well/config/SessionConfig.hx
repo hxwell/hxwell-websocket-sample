93af11828a8f98b9fde8829f547af8c8198aa854
barisyild <fb_bcy@hotmail.com>
2026-03-04 19:55:23 +0300

    Remove unused classes: AbortHandler, User, and update SessionConfig

--- src/hx/well/config/SessionConfig.hx
+++ src/hx/well/config/SessionConfig.hx
@@ -10,7 +10,5 @@ class SessionConfig implements IConfig {
     public var path:String = env("SESSION_PATH", "session");
     public var lifetime:Int = env("SESSION_LIFETIME", "360").parseInt();
 
-    public var guards:StringMap<Class<IAuthenticatable>> = [
-        "default" => hx.well.model.User
-    ];
+    public var guards:StringMap<Class<IAuthenticatable>> = [];
 }
